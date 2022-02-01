9952bbe79656dcb2363cc70f20463f395f0220ec
Sassi <joaopedro10813@gmail.com>
2022-02-01 16:50:02 -0300

    Simple Create form

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -18,5 +18,11 @@ namespace SalesWebMVC.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
